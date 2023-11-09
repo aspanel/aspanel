264816516cd6ecfa41e01eef14d7b90481577925
abhishekprajapati1 <ap0661236@gmail.com>
2023-11-09 14:29:11 +0530

    :fire: added coming soon pages

---
+++ app/services/[service]/page.tsx
@@ -0,0 +1,10 @@
+import ComingSoon from '@/components/ui/ComingSoon'
+import React from 'react'
+
+const ServiceDetailPage = () => {
+    return (
+        <ComingSoon />
+    )
+}
+
+export default ServiceDetailPage
