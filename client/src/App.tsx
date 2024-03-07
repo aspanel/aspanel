f0493046ed4addf8ad7b873bb757a77769fbb826
abhishekprajapati1 <ap0661236@gmail.com>
2024-03-07 18:41:06 +0530

    :fire: initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <Button variant="outline" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
