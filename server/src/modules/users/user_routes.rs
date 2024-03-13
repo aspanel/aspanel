0d943ae82e0a415a608c2eb7ab3bf36a7aa2f2af
abhishekprajapati1 <ap0661236@gmail.com>
2024-03-13 06:43:54 +0530

    :fire: added routes for users

--- server/src/modules/users/user_routes.rs
+++ server/src/modules/users/user_routes.rs
@@ -1,3 +1,11 @@
-use actix_web::web;
+use actix_web::{web, HttpResponse, Responder};
 
-pub fn user_routes(_cfg: &mut web::ServiceConfig) {}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello rust!")
+}
+
+
+pub fn user_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/users").route(web::get().to(index)));
+}
--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,10 +1,6 @@
-use crate::modules::users::user_routes::{self, user_routes};
-use actix_web::{web, Responder};
-
-async fn index() -> impl Responder {
-    "hello Rust"
-}
+use crate::modules::users::user_routes::user_routes;
+use actix_web::web;
 
 pub fn app_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/api").route("/hello", web::get().to(index)));
+    cfg.service(web::scope("/api").configure(user_routes));
 }
